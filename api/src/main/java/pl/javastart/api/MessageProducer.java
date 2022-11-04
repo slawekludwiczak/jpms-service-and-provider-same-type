33cb5316d0ac5c73dd65400dd1f7ba3a5238c529
slawek <slawekwch@gmail.com>
2022-11-04 12:18:47 +0100

    sample with provider as class instead of interface

--- api/src/main/java/pl/javastart/api/MessageProducer.java
+++ api/src/main/java/pl/javastart/api/MessageProducer.java
@@ -2,8 +2,4 @@ package pl.javastart.api;
 
 public interface MessageProducer {
     String getMessage();
-
-    public static MessageProducer provider() {
-        return new SimpleMessageProducer();
-    }
 }
--- api/src/main/java/pl/javastart/api/SimpleMessageProducer.java
+++ api/src/main/java/pl/javastart/api/SimpleMessageProducer.java
@@ -1,9 +1,14 @@
 package pl.javastart.api;
 
-class SimpleMessageProducer implements MessageProducer {
+public class SimpleMessageProducer implements MessageProducer {
+    private SimpleMessageProducer() { }
+
     @Override
     public String getMessage() {
         return "Simple Message";
     }
 
+    public static MessageProducer provider() {
+        return new SimpleMessageProducer();
+    }
 }
